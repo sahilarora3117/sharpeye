a6f57ee8c3f923b290afefa44fa07808e28e8075
sahilarora3117 <iamsahil@protonmail.com>
2020-04-13 22:43:42 +0530

    added cards

--- src/Components/Units/Unit/Unit.js
+++ src/Components/Units/Unit/Unit.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+    minWidth: 345,
+    float: 'left',
+    margin: '3em'
+  },
+
+
+  avatar: {
+    backgroundColor: red[500],
+  },
+
+}));
 const Unit = (props) => {
-    return (
-        <div>
-            <h2>{props.key}</h2>
-            <h1>{props.name}</h1>
-        </div>
-    )
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {props.name[0]}
+          </Avatar>
+        }
+        title={props.name}
+        subheader={props.code}
+      />
+      <CardContent>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites" onClick={() => console.log("Heart button Clicked")}>
+          <FavoriteIcon />
+        </IconButton>
+        <Button variant="contained" color="secondary" className="shownews" onClick={() => console.log("newsButton clicked")}>
+            See News
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
 
 export default Unit;
--- src/Components/Units/Units.js
+++ src/Components/Units/Units.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Unit from './Unit/Unit';
 
+const useStyles = () =>({
+    layout: {
+    },
+})
 const Units = (props) => {
-   
+    const classes = useStyles();
     return (
-        <div>
-            <h1>
-                {props.result.map ((element, index) => {
-                return <Unit key={element["Security Code"]} key={element["Security Code"]} name={element["Security Name"]} />;
-            })}
-            </h1>
+        <div className={classes.layout}>
+                {props.result
+                    .map ((element, index) => {
+                        return <Unit key={element["Security Code"]} name={element["Security Name"]} code={element["Security Code"]}/>;
+                })}
         </div>
     )
 } 
 
 export default Units
+
